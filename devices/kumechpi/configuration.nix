d71f55e3473328e7cf64519c7aae5fc58c4bd485
Ameer Taweel <ameertaweel2002@gmail.com>
2023-11-07 09:02:01 +0300

    feat: base kumechpi config

---
+++ devices/kumechpi/configuration.nix
@@ -0,0 +1,72 @@
+{
+  params,
+  pkgs,
+  ...
+}: {
+  imports = [./hardware-configuration.nix];
+
+  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  # Headless / No Graphical Session
+  services.xserver.enable = false;
+  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.desktopManager.gnome.enable = false;
+
+  # Hostname
+  networking.hostName = params.hostname;
+
+  # Timezone
+  time.timeZone = params.timezone;
+
+  # Internationalisation
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+
+  # Define user account
+  # Set a password with `passwd`
+  users.users.${params.username} = {
+    isNormalUser = true;
+    extraGroups = ["networkmanager" "wheel" "video"];
+    initialPassword = params.username;
+  };
+
+  # Packages installed in system profile
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+
+  # This option defines the first version of NixOS you have installed on this
+  # particular machine, and is used to maintain compatibility with application
+  # data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for
+  # any reason, even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are
+  # pulled from, so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your
+  # system is out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes
+  # it would make to your configuration, and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or
+  # https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
+  system.stateVersion = params.state-version; # Did you read the comment?
+}
---
+++ devices/kumechpi/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "KUMechPI (Raspberry Pi 4 Model B Rev 1.5) Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    params = {
+      system = "aarch64-linux";
+      hostname = "kumechpi";
+      username = "user";
+      timezone = "Asia/Istanbul";
+      state-version = "23.11";
+    };
+  in {
+    nixosConfigurations.${params.hostname} = nixpkgs.lib.nixosSystem {
+      inherit (params) system;
+      specialArgs = {inherit inputs outputs params;};
+      modules = [./configuration.nix];
+    };
+  };
+}
