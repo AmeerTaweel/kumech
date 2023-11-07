2dfc7f9656e11e043bde6a05cd5159f3fc6f2773
Ameer Taweel <ameertaweel2002@gmail.com>
2023-11-08 01:00:31 +0300

    feat: add development packages to shell.nix

--- shell.nix
+++ shell.nix
@@ -3,6 +3,24 @@
 {pkgs ? (import ./nixpkgs.nix) {}}: {
   default = pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
+      # Command Runner
+      just
+
+      # Network Tool
+      nmap
+
+      # NixOS SD Card Image Compression / Decompression
+      zstd
+
+      # Compress and Convert Videos
+      # https://handbrake.fr/
+      handbrake
+
+      # Raspberry PI
+      rpi-imager
+
+      # Ground Control Software
+      qgroundcontrol
     ];
   };
 }
